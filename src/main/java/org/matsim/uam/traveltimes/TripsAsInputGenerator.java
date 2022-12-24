ff7927e4d9a7a1167eae63aef075f01b616ff6a7
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2022-12-24 16:59:52 +0100

    new script for generate trip.csv for travel time analysis with MATSim-UAM

---
+++ src/main/java/org/matsim/uam/traveltimes/TripsAsInputGenerator.java
@@ -0,0 +1,91 @@
+package org.matsim.uam.traveltimes;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+import org.matsim.core.utils.misc.Time;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+/**
+ * @author haowu
+ */
+public class TripsAsInputGenerator {
+    private static boolean writeDescription = true;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(
+                "ARGS: trips.csv* outputfile-name.csv* write-description");
+        System.out.println("(* required)");
+
+        // ARGS
+        int j = 0;
+        Path inputTripsPath = Path.of(args[j++]);
+        Path outputTripsPath = Path.of(args[j++]);
+
+        if (args.length > 2)
+            writeDescription = Boolean.parseBoolean(args[j]);
+
+
+
+        //process trips
+        CSVPrinter csvWriter = new CSVPrinter(new FileWriter(outputTripsPath.toString()), CSVFormat.DEFAULT.withDelimiter(',').withFirstRecordAsHeader());
+        List<String> tripsTitleRow = Arrays.asList
+                ("from_x", "from_y", "to_x", "to_y", "start_time");
+        csvWriter.printRecord(tripsTitleRow);
+
+        int numOfTripsServed = 0;
+        try (CSVParser parser = new CSVParser(Files.newBufferedReader(inputTripsPath),
+                CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader())) {
+            for (CSVRecord record : parser.getRecords()) {
+                double fromX = Double.parseDouble(record.get("start_x"));
+                double fromY = Double.parseDouble(record.get("start_y"));
+                double toX = Double.parseDouble(record.get("end_x"));
+                double toY = Double.parseDouble(record.get("end_y"));
+                CustomTimeWriter customTimeWriter = new DefaultTimeWriter();
+                double departureTime = Time.parseTime(record.get("dep_time"));
+
+
+                //write output
+                List<String> outputRow = new ArrayList<>();
+
+                outputRow.add(Double.toString(fromX));
+                outputRow.add(Double.toString(fromY));
+                outputRow.add(Double.toString(toX));
+                outputRow.add(Double.toString(toY));
+                outputRow.add(customTimeWriter.writeTime(departureTime));
+
+
+                csvWriter.printRecord(outputRow);
+
+                numOfTripsServed++;
+            }
+        }
+        csvWriter.close();
+        System.out.println("numOfTripsServed: " + numOfTripsServed);
+
+    }
+
+    public interface CustomTimeWriter {
+        String writeTime(double time);
+    }
+
+    static class SecondsFromMidnightTimeWriter implements CustomTimeWriter {
+        @Override
+        public String writeTime(double time) {
+            return Long.toString((long) time);
+        }
+    }
+
+    static class DefaultTimeWriter implements CustomTimeWriter {
+        @Override
+        public String writeTime(double time) {
+            return Time.writeTime(time);
+        }
+    }
+}
