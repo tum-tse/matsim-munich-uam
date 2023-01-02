e9489fb43a925006bcee57bb7f566fecd8d1621e
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2023-01-02 18:35:07 +0100

    class includes bug

--- src/main/java/org/matsim/uam/landUse/CreateLandUseShp.java
+++ src/main/java/org/matsim/uam/landUse/CreateLandUseShp.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-
+@Deprecated
 public class CreateLandUseShp {
     private static final Logger log = LogManager.getLogger(CreateLandUseShp.class);
     private static final Set<String> INCLUDE = Set.of(
