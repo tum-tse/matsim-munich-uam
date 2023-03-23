2ad02bdc97f208bcab267019fecdea44517de42e
Hao Wu <56875899+haowuintub@users.noreply.github.com>
2023-03-23 15:06:46 +0100

    fix a bug after: add reference trips.csv which includes the zone id

--- src/main/java/org/matsim/uam/prepare/population/GenerateDummyZoneBasedDemand.java
+++ src/main/java/org/matsim/uam/prepare/population/GenerateDummyZoneBasedDemand.java
@@ -93,7 +93,7 @@ public class GenerateDummyZoneBasedDemand {
                 csvWriter.printRecord(outputRow);
 
                 //for the reference csv
-                outputRow.add(Double.toString((double) feature.getAttributes().get( 1 )));
+                outputRow.add(Integer.toString((int) feature.getAttributes().get( 1 )));
                 if (tripsTitleRow2.size() != outputRow.size()) {
                     throw new RuntimeException("tripsTitleRow2.size() != outputRow.size()");
                 }
@@ -120,7 +120,7 @@ public class GenerateDummyZoneBasedDemand {
                 csvWriter.printRecord(outputRow);
 
                 //for the reference csv
-                outputRow.add(Double.toString((double) feature.getAttributes().get( 1 )));
+                outputRow.add(Integer.toString((int) feature.getAttributes().get( 1 )));
                 if (tripsTitleRow2.size() != outputRow.size()) {
                     throw new RuntimeException("tripsTitleRow2.size() != outputRow.size()");
                 }
